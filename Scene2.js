01c4d30f33e71845f7abdee6adfa4799169574d4
Chao Zhou <Czhou0k@gmail.com>
2023-02-25 13:24:47 -0500

    got ship to move

--- Scene2.js
+++ Scene2.js
@@ -28,4 +28,10 @@ class Scene2 extends Phaser.Scene {
   moveShip(ship, speed) {
     ship.y += speed;
   }
+
+  update() {
+    this.moveShip(this.ship1, 1);
+    this.moveShip(this.ship2, 2);
+    this.moveShip(this.ship3, 3);
+  }
 }
