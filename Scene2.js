8a3979c5439f386984807bcf00f32b7e63ddfaac
Chao Zhou <Czhou0k@gmail.com>
2023-02-25 13:48:28 -0500

    got a working moving background image

--- Scene2.js
+++ Scene2.js
@@ -5,6 +5,13 @@ class Scene2 extends Phaser.Scene {
 
   create() {
     this.background = this.add.image(0, 0, 'background');
+    this.background = this.add.tileSprite(
+      0,
+      0,
+      config.width,
+      config.height,
+      'background'
+    );
     this.background.setOrigin(0, 0);
 
     this.ship1 = this.add.image(
@@ -25,6 +32,14 @@ class Scene2 extends Phaser.Scene {
     });
   }
 
+  update() {
+    this.moveShip(this.ship1, 1);
+    this.moveShip(this.ship2, 2);
+    this.moveShip(this.ship3, 3);
+
+    this.background.tilePositionY -= 0.5;
+  }
+
   moveShip(ship, speed) {
     ship.y += speed;
     if (ship.y > config.height) {
@@ -36,11 +51,5 @@ class Scene2 extends Phaser.Scene {
     ship.y = 0;
     var randomX = Phaser.Math.Between(0, config.width);
     ship.x = randomX;
-  }
-
-  update() {
-    this.moveShip(this.ship1, 1);
-    this.moveShip(this.ship2, 2);
-    this.moveShip(this.ship3, 3);
   }
 }
