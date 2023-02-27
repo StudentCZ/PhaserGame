ba0fa9e47947b1e12dd04c0d51bc755ad2e6e37c
Chao Zhou <Czhou0k@gmail.com>
2023-02-27 17:11:08 -0500

    moved animations code to scene1 for cleaner code

--- Scene1.js
+++ Scene1.js
@@ -35,5 +35,53 @@ class Scene1 extends Phaser.Scene {
   create() {
     this.add.text(20, 20, 'Loading game...');
     this.scene.start('playGame');
+
+    this.anims.create({
+      key: 'ship1_anim',
+      frames: this.anims.generateFrameNumbers('ship'),
+      frameRate: 20,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: 'ship2_anim',
+      frames: this.anims.generateFrameNumbers('ship2'),
+      frameRate: 20,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: 'ship3_anim',
+      frames: this.anims.generateFrameNumbers('ship3'),
+      frameRate: 20,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: 'explosion',
+      frames: this.anims.generateFrameNumbers('explosion'),
+      frameRate: 20,
+      repeat: 0,
+      hideOnComplete: true,
+    });
+
+    this.anims.create({
+      key: 'red',
+      frames: this.anims.generateFrameNumbers('power-up', {
+        start: 0,
+        end: 1,
+      }),
+      frameRate: 20,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: 'gray',
+      frames: this.anims.generateFrameNumbers('power-up', {
+        start: 2,
+        end: 3,
+      }),
+      frameRate: 20,
+      repeat: -1,
+    });
   }
 }
