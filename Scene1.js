ec1b4ec24bf84f9e230c07d4c0729fabd0c9fd38
Chao Zhou <Czhou0k@gmail.com>
2023-02-27 18:16:55 -0500

    added beam sprite and animation to scene 1 and asset folder

--- Scene1.js
+++ Scene1.js
@@ -30,6 +30,10 @@ class Scene1 extends Phaser.Scene {
       frameWidth: 16,
       frameHeight: 24,
     });
+    this.load.spritesheet('beam', 'assets/spritesheets/beam.png', {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
   }
 
   create() {
@@ -89,6 +93,12 @@ class Scene1 extends Phaser.Scene {
         frameRate: 20,
         repeat: -1,
       }),
+    });
+    this.anims.create({
+      key: 'beam_anim',
+      frames: this.anims.generateFrameNumbers('beam'),
+      frameRate: 20,
+      repeat: -1,
     });
   }
 }
