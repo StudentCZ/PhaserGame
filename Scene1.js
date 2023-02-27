59992cb71049e94dbeda62d2169d8ccacba9804f
Chao Zhou <Czhou0k@gmail.com>
2023-02-27 16:41:27 -0500

    got power-ups to display in game screen

--- Scene1.js
+++ Scene1.js
@@ -31,24 +31,5 @@ class Scene1 extends Phaser.Scene {
   create() {
     this.add.text(20, 20, 'Loading game...');
     this.scene.start('playGame');
-
-    this.anims.create({
-      key: 'red',
-      frames: this.anims.generateFrameNumbers('power-up', {
-        start: 0,
-        end: 1,
-      }),
-      frameRate: 20,
-      repeat: -1,
-    });
-    this.anims.create({
-      key: 'gray',
-      frames: this.anims.generateFrameNumbers('power-up', {
-        start: 2,
-        end: 3,
-      }),
-      frameRate: 20,
-      repeat: -1,
-    });
   }
 }
--- Scene2.js
+++ Scene2.js
@@ -55,6 +55,33 @@ class Scene2 extends Phaser.Scene {
       hideOnComplete: true,
     });
 
+    this.anims.create({
+      key: 'red',
+      frames: this.anims.generateFrameNumbers('power-up', {
+        start: 0,
+        end: 1,
+      }),
+      frameRate: 20,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: 'gray',
+      frames: this.anims.generateFrameNumbers('power-up', {
+        start: 2,
+        end: 3,
+      }),
+      frameRate: 20,
+      repeat: -1,
+    });
+    this.powerUps = this.physics.add.group();
+
+    var maxObjects = 5;
+    for (var i = 0; i <= maxObjects; i++) {
+      var powerUp = this.physics.add.sprite(16, 16, 'power-up');
+      this.powerUps.add(powerUp);
+      powerUp.setRandomPosition(0, 0, game.config.width, game.config.height);
+    }
+
     this.add.text(20, 20, 'Playing Game', {
       font: '25px Arial',
       fill: 'yellow',
